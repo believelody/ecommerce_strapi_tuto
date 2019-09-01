b69f29baade006323f1146c92a797ffa888c9ed0
believelody <believelody@gmail.com>
2019-09-01 11:06:22 +0200

    update webpack to handle react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+// import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import { Router, Link } from '@reach/router'
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import CheckoutPage from './pages/CheckoutPage';
 
 const App = () => {
   return (
     <div>
-      Eyindaka mabe
+      <nav style={{ border: '1px solid red' }}>
+        <Link to='/login'>Login</Link>{' '}
+        <Link to='/register'>Register</Link>{' '}
+        <Link to='/checkout'>Checkout</Link>
+      </nav>
+      <Router>
+        <HomePage exact path='/' />
+        <LoginPage path='/login' />
+        <RegisterPage path='/register' />
+        <CheckoutPage path='/checkout' />
+      </Router>
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
     entry:'./src/index.js',
     output: {
         path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js'
+        filename: 'index_bundle.js',
+        publicPath: '/'
     },
     module: {
         rules: [
@@ -17,6 +18,9 @@ module.exports = {
                 }
             }
         ]
+    },
+    devServer: {
+        historyApiFallback: true
     },
     plugins: [
         new HtmlWebpackPlugin({
