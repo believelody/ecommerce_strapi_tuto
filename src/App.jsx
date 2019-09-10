0860abd93bbce25c2e341fc9f70b6688f338f014
believelody <believelody@gmail.com>
2019-09-10 17:07:14 +0200

    Design loader when lauched

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Router } from '@reach/router'
 import styled from 'styled-components'
-import { Container } from 'gestalt'
+import { Container, Spinner } from 'gestalt'
 
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import CheckoutPage from './pages/CheckoutPage'
 import Navbar from './components/navbar/Navbar'
+import { useAppHooks } from './contexts'
+import { SwappingSquaresSpinner, RadarSpinner } from 'react-epic-spinners'
 
 const AppStyle = styled.div`
   ul {
@@ -17,9 +19,28 @@ const AppStyle = styled.div`
   a {
     text-decoration: none;
   }
+
+  .dimmer {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, .6);
+  }
+
+  .spinner {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate3d(-50%, -50%, 0);
+  }
 `
 
 const App = () => {
+  const { useLoading } = useAppHooks()
+
+  const [{ loading }, dispatchLoading] = useLoading
 
   return (
     <AppStyle>
@@ -31,6 +52,13 @@ const App = () => {
           <RegisterPage path='/register' />
           <CheckoutPage path='/checkout' />
         </Router>
+        {/* <Spinner show={loading} accessibilityLabel='Loading Spinner' /> */}
+        {
+          loading &&
+          <div className="dimmer">
+            <RadarSpinner className='spinner' color='deepskyblue' />
+          </div>
+        }
       </Container>
     </AppStyle>
   )
